3d3949c8658562a4e65d2ed3f8379d07763f8d10
altamiranomatias00-lab <altamirano.matias00@gmail.com>
2026-04-08 16:12:53 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from ..routes.servicios import router as servicios_router
-from ..routes.auth import router as auth_router
+from routes.servicios import router as servicios_router
+from routes.auth import router as auth_router
 
 app = FastAPI()
 
